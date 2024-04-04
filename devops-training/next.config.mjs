908b2c86a7e4965eabe04352c3e4a35c2e4dccb5
Leticia <leticia.cohen@inspira.com.br>
2024-04-04 17:03:30 -0300

    [front] add env variable

--- devops-training/next.config.mjs
+++ devops-training/next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+// next.config.mjs
+import { config } from 'dotenv'
+config()
+
+const nextConfig = {
+  // Sua configuração Next.js aqui
+  env: {
+    BASE_URL: process.env.BASE_URL,
+  },
+}
 
 export default nextConfig;
